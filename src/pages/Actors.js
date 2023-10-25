d12e73a10bed6c2f64ffc10a14033026b204057d
chaya <chayavogel1@gmail.com>
2023-10-25 12:14:40 -0400

    complete lab

--- src/pages/Actors.js
+++ src/pages/Actors.js
@@ -1,13 +1,38 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
 
 function Actors() {
+
+  const [actors, setActors] = useState([])
+
+  useEffect(() =>{
+    fetch("http://localhost:4000/actors")
+      .then(r => r.json())
+      .then(data => setActors(data))
+      .catch(error => console.error(error))
+  }, [])
+
+  const info = actors && actors.map((actor) => {
+    return (
+      <article key={`article-${actor.id}`}>
+        <h2 key={`h2-${actor.id}`}>{actor.name}</h2>
+        <ul>
+        {actor.movies.map((movie) => {
+          return <li key={`ul-${movie}`}>{movie}</li>
+        })}
+        </ul>
+      </article>
+    )
+  })
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Actor info here! */}
+        <h1>Actors Page</h1>
+        {info}
       </main>
     </>
   );
--- src/pages/Directors.js
+++ src/pages/Directors.js
@@ -1,13 +1,38 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
 
 function Directors() {
+
+  const [directors, setDirectors] = useState([])
+
+  useEffect(() =>{
+    fetch("http://localhost:4000/directors")
+      .then(r => r.json())
+      .then(data => setDirectors(data))
+      .catch(error => console.error(error))
+  }, [])
+
+  const info = directors && directors.map((director) => {
+    return (
+      <article key={`article-${director.id}`}>
+        <h2 key={`h2-${director.id}`}>{director.name}</h2>
+        <ul>
+        {director.movies.map((movie) => {
+          return <li key={`ul-${movie}`}>{movie}</li>
+        })}
+        </ul>
+      </article>
+    )
+  })
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Director info here! */}
+        <h1>Directors Page</h1>
+        {info}
       </main>
     </>
   );
